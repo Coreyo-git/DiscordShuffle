9b618cf881b52c6184c6fc0847c46732c03afd57
coreyo-git <corey.oconnor@live.com.au>
2022-11-27 23:26:12 +1000

    added button to startshuffle command

--- Bot/commands/startshuffle.js
+++ Bot/commands/startshuffle.js
@@ -1,10 +1,22 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  Events,
+} = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('startshuffle')
-		.setDescription('begins a new shuffle event'),
-	async execute(interaction) {
-		await interaction.reply('React to this with a tick to include yourself!');
-	},
+  data: new SlashCommandBuilder()
+    .setName("startshuffle")
+    .setDescription("begins a new shuffle event"),
+  async execute(interaction) {
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("primary")
+        .setLabel("Sign up!")
+        .setStyle(ButtonStyle.Primary)
+    );
+	await interaction.reply({ content: 'Sign up for the shuffle!', components: [row] });
+  },
 };
--- Bot/events/interactionCreate.js
+++ Bot/events/interactionCreate.js
@@ -1,28 +1,36 @@
-const { Events } = require("discord.js");
+const {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    Events,
+} = require("discord.js");
 
 // event that will execute code when your application receives an interaction
 module.exports = {
-  name: Events.InteractionCreate,
-  async execute(interaction) {
-    if (!interaction.isChatInputCommand()) return;
-    // logs the interaction for info
-    // console.log(interaction);
+    name: Events.InteractionCreate,
+    async execute(interaction) {
+        if (!interaction.isChatInputCommand()) return;
+        // logs the interaction for info
+        // console.log(interaction);
 
-	// gets the commands interactions
-    const command = interaction.client.commands.get(interaction.commandName);
+        // gets the commands interactions
+        const command = interaction.client.commands.get(
+            interaction.commandName
+        );
 
-    if (!command) {
-      console.error(
-        `No command matching ${interaction.commandName} was found.`
-      );
-      return;
-    }
+        if (!command) {
+            console.error(
+                `No command matching ${interaction.commandName} was found.`
+            );
+            return;
+        }
 
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(`Error executing ${interaction.commandName}`);
-      console.error(error);
-    }
-  },
+        try {
+            // executes the specific command
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(`Error executing ${interaction.commandName}`);
+            console.error(error);
+        }
+    },
 };
