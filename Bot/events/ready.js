ba2bcd9782e0cbf955f24339e38e6382c507622a
coreyo-git <corey.oconnor@live.com.au>
2022-11-27 22:56:56 +1000

    Seperated event class to its own subdirectory

---
+++ Bot/events/ready.js
@@ -0,0 +1,10 @@
+const { Events } = require('discord.js');
+
+// When the client is ready, run this code
+module.exports = {
+	name: Events.ClientReady,
+	once: true,
+	execute(client) {
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+	},
+};
